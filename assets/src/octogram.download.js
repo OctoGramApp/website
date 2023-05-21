f56a2bd138061b74b51fef88ce71ba8b3ee5b52e
Giovanni <appcounts47@gmail.com>
2023-05-21 21:19:17 +0200

    Bug fixes and other improvements

---
+++ assets/src/octogram.download.js
@@ -0,0 +1,20 @@
+window.addEventListener('load', () => {
+  const downloadFiles = document.querySelector('body .download .content .files');
+  if (downloadFiles != null) {
+    const selectElement = downloadFiles.querySelector('select');
+    const buttonElement = downloadFiles.querySelector('.button');
+    if (selectElement != null && buttonElement != null) {
+      buttonElement.addEventListener('click', () => {
+        let finalUrl = 'https://github.com';
+        finalUrl += '/OctoGramApp';
+        finalUrl += '/OctoGram';
+        finalUrl += '/releases';
+        finalUrl += '/download';
+        finalUrl += '/%23stable';
+        finalUrl += '/'+selectElement.value;
+
+        window.location.href = finalUrl;
+      });
+    }
+  }
+});
