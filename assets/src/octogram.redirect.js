a19a84ce4d03c4a5f42e85f996ab3ed8277a0e50
Giovanni <appcounts47@gmail.com>
2023-05-28 21:59:03 +0200

    Bug fixes and other improvements

--- assets/src/octogram.redirect.js
+++ assets/src/octogram.redirect.js
@@ -4,24 +4,22 @@ window.addEventListener('load', () => {
     case '/appcenter-beta':
     case '/acbeta':
     case '/beta':
-      window.location.href = composeUrl('Octogram-Beta');
+      window.location.href = composeUrl('octogram-beta');
     break;
     case '/appcenter_stable':
     case '/appcenter-stable':
     case '/ac':
     case '/acstable':
     case '/stable':
-      window.location.href = composeUrl('Octogram');
+      window.location.href = composeUrl('octogram');
     break;
   }
 });
 
 function composeUrl(appName) {
   let myUrl = 'https://install.appcenter.ms';
-  myUrl += '/api/v0.1/apps/octogramapp/';
+  myUrl += '/orgs/octogramapp/apps/';
   myUrl += appName;
   myUrl += '/distribution_groups/app';
-  myUrl += '/releases/latest';
-  myUrl += '?is_install_page=true';
   return myUrl;
 }
