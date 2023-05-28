f4f81a58b30135bd9e415fea55b8d39be450db17
Giovanni <appcounts47@gmail.com>
2023-05-28 21:49:08 +0200

    Improved short links

---
+++ assets/src/octogram.redirect.js
@@ -0,0 +1,27 @@
+window.addEventListener('load', () => {
+  switch(window.location.pathname) {
+    case '/appcenter_beta':
+    case '/appcenter-beta':
+    case '/acbeta':
+    case '/beta':
+      window.location.href = tryToGetVersion('Octogram-Beta');
+    break;
+    case '/appcenter_stable':
+    case '/appcenter-stable':
+    case '/ac':
+    case '/acstable':
+    case '/stable':
+      window.location.href = tryToGetVersion('Octogram');
+    break;
+  }
+});
+
+function composeUrl(appName) {
+  let myUrl = 'https://install.appcenter.ms';
+  myUrl += '/api/v0.1/apps/octogramapp/';
+  myUrl += appName;
+  myUrl += '/distribution_groups/app';
+  myUrl += '/releases/latest';
+  myUrl += '?is_install_page=true';
+  return myUrl;
+}
