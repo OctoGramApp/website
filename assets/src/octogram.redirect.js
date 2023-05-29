d165887925dba305f0ead357a3374fa490d962da
Giovanni <appcounts47@gmail.com>
2023-05-29 19:42:49 +0200

    Added new short links

--- assets/src/octogram.redirect.js
+++ assets/src/octogram.redirect.js
@@ -1,20 +1,29 @@
-window.addEventListener('load', () => {
-  switch(window.location.pathname) {
-    case '/appcenter_beta':
-    case '/appcenter-beta':
-    case '/acbeta':
-    case '/beta':
-      window.location.href = composeUrl('octogram-beta');
-    break;
-    case '/appcenter_stable':
-    case '/appcenter-stable':
-    case '/ac':
-    case '/acstable':
-    case '/stable':
-      window.location.href = composeUrl('octogram');
-    break;
-  }
-});
+switch(window.location.pathname) {
+  case '/appcenter_beta':
+  case '/appcenter-beta':
+  case '/acbeta':
+  case '/beta':
+    window.location.href = composeUrl('octogram-beta');
+  break;
+  case '/appcenter_stable':
+  case '/appcenter-stable':
+  case '/ac':
+  case '/acstable':
+  case '/stable':
+    window.location.href = composeUrl('octogram');
+  break;
+  case '/apkpure':
+    window.location.href = 'https://apkpure.com/octogram/it.octogram.android';
+  break;
+  case '/telegram':
+  case '/tg':
+    window.location.href = 'https://t.me/octogramapp';
+  break;
+  case '/github':
+  case '/gh':
+    window.location.href = 'https://github.com/octogramapp';
+  break;
+}
 
 function composeUrl(appName) {
   let myUrl = 'https://install.appcenter.ms';
